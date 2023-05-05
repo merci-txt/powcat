b32513fd7b565d9c7c308903118aa66c7fe2ea8e
merci-txt <merci.txt@proton.me>
2023-05-05 11:14:46 +0100

    added basic functionality

--- powcat.py
+++ powcat.py
@@ -9,15 +9,29 @@ ssl_help = 'Support for ssl-encryption. Only respected when protocol is http. De
 anonymity_help = 'Proxy-anonymity. Defaults to all.'
 test_help = 'Test proxies after retrieving. Use as flag.'
 
-parser = argparse.ArgumentParser(prog='powcat', description=description)
+logo = '''
+88""Yb  dP"Yb  Yb        dP  dP""b8    db    888888 
+88__dP dP   Yb  Yb  db  dP  dP   `"   dPYb     88   
+88"""  Yb   dP   YbdPYbdP   Yb       dP__Yb    88   
+88      YbodP     YP  YP     YboodP dP""""Yb   88 
 
-parser.add_argument('--protocol', help=protocol_help, action='store', choices=['http', 'socks4', 'socks5'], required=True)
-parser.add_argument('--country', help=country_help, action='store', default='all', required=False)
-parser.add_argument('--timeout', help=timeout_help, action='store', type=int, default=250, required=False)
-parser.add_argument('--ssl', help=ssl_help, action='store', choices=['yes', 'no', 'all'], default='all', required=False)
-parser.add_argument('--anonymity', help=anonymity_help, action='store', choices=['transparent', 'anonymous', 'elite', 'all'], default='all', required=False)
-parser.add_argument('--test', help=test_help, action='store_true', required=False)
+v1 by merci.txt
+'''
 
-args = parser.parse_args()
+if __name__ == '__main__':
+    print(logo)
 
-print(scraper.scrape(args.protocol, args.timeout, args.country, args.ssl, args.anonymity))
+    parser = argparse.ArgumentParser(prog='powcat', description=description)
+    parser.add_argument('--protocol', help=protocol_help, action='store', choices=['http', 'socks4', 'socks5'], required=True)
+    parser.add_argument('--country', help=country_help, action='store', default='all', required=False)
+    parser.add_argument('--timeout', help=timeout_help, action='store', type=int, default=250, required=False)
+    parser.add_argument('--ssl', help=ssl_help, action='store', choices=['yes', 'no', 'all'], default='all', required=False)
+    parser.add_argument('--anonymity', help=anonymity_help, action='store', choices=['transparent', 'anonymous', 'elite', 'all'], default='all', required=False)
+    parser.add_argument('--test', help=test_help, action='store_true', required=False)
+    args = parser.parse_args()
+
+    print('[Scraping proxies]')
+    proxies = scraper.scrape(args.protocol, args.timeout, args.country, args.ssl, args.anonymity)
+
+    for proxy in proxies:
+        print(proxy)
